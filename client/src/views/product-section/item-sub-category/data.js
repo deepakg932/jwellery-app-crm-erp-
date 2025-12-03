36ecf021eac02668a0cb2eea061ad94163fe5e1c
TechAyush <techpanda354@gmail.com>
2025-12-03 11:45:53 +0530

    first commit for frontend side

---
+++ client/src/views/product-section/item-sub-category/data.js
@@ -0,0 +1,56 @@
+// data.js - You can use this to separate your data
+export const parentCategories = [
+  { id: 1, name: "Rings" },
+  { id: 2, name: "Necklaces" },
+  { id: 3, name: "Bracelets" },
+  { id: 4, name: "Earrings" },
+  { id: 5, name: "Nose Pins" },
+  { id: 6, name: "Anklets" },
+  { id: 7, name: "Brooches" },
+  { id: 8, name: "Pendants" },
+  { id: 9, name: "Charms" },
+  { id: 10, name: "Cufflinks" },
+];
+
+export const initialSubCategories = [
+  { 
+    id: 1, 
+    name: "Diamond Rings", 
+    categoryId: 1, 
+    categoryName: "Rings", 
+    image: null,
+    description: "Premium diamond engagement rings"
+  },
+  { 
+    id: 2, 
+    name: "Gold Chains", 
+    categoryId: 2, 
+    categoryName: "Necklaces", 
+    image: null,
+    description: "Various gold chain designs"
+  },
+  { 
+    id: 3, 
+    name: "Silver Bangles", 
+    categoryId: 3, 
+    categoryName: "Bracelets", 
+    image: null,
+    description: "Traditional silver bangles"
+  },
+  { 
+    id: 4, 
+    name: "Platinum Earrings", 
+    categoryId: 4, 
+    categoryName: "Earrings", 
+    image: null,
+    description: "Platinum stud and drop earrings"
+  },
+  { 
+    id: 5, 
+    name: "Gold Nose Pins", 
+    categoryId: 5, 
+    categoryName: "Nose Pins", 
+    image: null,
+    description: "Traditional Indian nose pins"
+  },
+];
